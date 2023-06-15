28b4186a5c2c08a4a97a57cf0772b53042c26f91
Hertz <Absolab@github.com>
2023-06-15 16:06:05 +0900

    반환 객체 기본 생성자 제거

--- server/src/main/java/kr/co/dtaas/test/board/responseObject/ResponseObject.java
+++ server/src/main/java/kr/co/dtaas/test/board/responseObject/ResponseObject.java
@@ -4,10 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @JsonInclude(Include.NON_NULL)
 public abstract class ResponseObject {
 
