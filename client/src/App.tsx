7bfc94dbd258367ef9777c4ea354ca5141f4a787
Hertz <Absolab@github.com>
2023-06-15 16:57:16 +0900

    client-side 초기 세팅

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* css test */}
+        <div className='w-80 h-80 bg-black'></div>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
