c50dfcf0b9383d54f3da08394f705e6d1a177833
Hertz <kljj0650@naver.com>
2023-06-17 23:13:08 +0900

    api 추가

--- client/src/commons/apis.tsx
+++ client/src/commons/apis.tsx
@@ -10,6 +10,8 @@ const config = () => {
 }
 
 const apis = {
+
+    // 로그인
     postLogin: async (id:string, pwd:string) => {
 
         const data = new FormData();
@@ -19,16 +21,37 @@ const apis = {
         try {
             let res = await axios.post(config().url + ':' + config().port + '/users/login', data, config().config);
             return res.data;
-        } catch (_) { }
+        } catch (_) { console.log(_); }
     },
 
+    // 로그인 확인
     getIsLogined: async () => {
         try {
             const res = await axios.get(config().url + ':' + config().port + '/users/login', config().config);
-            return res.data
-        } catch (_) { }
-    }
+            return res.data;
+        } catch (_) { console.log(_); }
+    },
 
+    // 글 목록
+    getList: async () => {
+        try {
+            const res = await axios.get(config().url + ':' + config().port + '/board/list', config().config);
+            return res.data;
+        } catch (_) { console.log(_) }
+    },
+
+    // 글 등록
+    postWrite: async (title:string, content:string) => {
+
+        const data = new FormData();
+        data.append('title', title);
+        data.append('content', content);
+
+        try {
+            const res = await axios.post(config().url + ':' + config().port + '/board/write', data, config().config);
+            return res.data;
+        } catch (_) { console.log (_); }
+    },
 }
 
 export default apis;
