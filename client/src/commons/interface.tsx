3068e90da23dd9a2d49750ff6dfda6b77168e6ca
Hertz <kljj0650@naver.com>
2023-06-17 23:13:18 +0900

    response 인터페이스 추가

--- client/src/commons/interface.tsx
+++ client/src/commons/interface.tsx
@@ -5,3 +5,22 @@ export interface WriteInterface {
     writer?: string,
     date?: string
 }
+
+export const ResponseResultValue = {
+    SUCCESS: 'SUCCESS',
+    FAIL: 'FAIL',
+}
+
+export interface ResponseInterface {
+    result: string,
+    msg: string,
+    data: Object,
+}
+
+export interface LoginResponseInterface extends ResponseInterface {
+    data: {
+        id: string,
+        name: string,
+        reg_date: string,
+    }
+}
