f69b0179c38d07513e09e5453380626282cbe4e4
Hertz <kljj0650@naver.com>
2023-06-18 02:51:39 +0900

    더미데이터 추가

--- db/dummy.sql
+++ db/dummy.sql
@@ -4,3 +4,7 @@ INSERT INTO tb_user(id, pwd, name) VALUES ('admin', SHA2('admin', 256), 'admin')
 INSERT INTO tb_user(id, pwd, name) VALUES ('test1', SHA2('test1', 256), 'test1');
 INSERT INTO tb_user(id, pwd, name) VALUES ('test2', SHA2('test2', 256), 'test2');
 INSERT INTO tb_user(id, pwd, name) VALUES ('test3', SHA2('test3', 256), 'test3');
+
+INSERT INTO tb_board(uid, title, content) VALUES (1, 'title1', 'contents1');
+INSERT INTO tb_board(uid, title, content) VALUES (1, 'title2', 'contents2');
+INSERT INTO tb_board(uid, title, content) VALUES (1, 'title3', 'contents3');
