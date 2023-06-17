9c8419985daf0fe3b83bac00a35e2dc8e3d8a7af
Hertz <kljj0650@naver.com>
2023-06-18 02:40:14 +0900

    에러 코드 수정

--- server/src/main/java/kr/co/dtaas/test/board/responseObject/LoginResponseObject.java
+++ server/src/main/java/kr/co/dtaas/test/board/responseObject/LoginResponseObject.java
@@ -4,19 +4,16 @@ import kr.co.dtaas.test.board.dto.UserDto;
 
 public class LoginResponseObject extends ResponseObject {
 
-    // Login
-    public static final int LOGIN_SUCCESS    = 1000;
-    public static final int ALREADY_LOGGED_IN  = 1001;
-    public static final int INVALID_PASSWORD = 1002;
+    // SUCCESS
+    public static final int LOGIN_SUCCESS      = 1000;  // login
+    public static final int LOGOUT_SUCCESS     = 1001;  // logout
+    public static final int IS_LOGGED_IN       = 1002;  // check login
 
-    // Logout
-    public static final int LOGOUT_SUCCESS   = 2000;
-    public static final int NOT_LOGGED_IN    = 2001;
-
-    // IsLogined
-    public static final int IS_LOGGED_IN     = 3000;
-    public static final int IS_NOT_LOGGED_IN = 3001;
-
+    // FAIL
+    public static final int ALREADY_LOGGED_IN  = 2001;  // login
+    public static final int INVALID_PASSWORD   = 2002;  // login
+    public static final int NOT_LOGGED_IN      = 2003;  // logout
+    public static final int IS_NOT_LOGGED_IN   = 2004;  // check login
 
     public LoginResponseObject(int code, UserDto user) {
 
