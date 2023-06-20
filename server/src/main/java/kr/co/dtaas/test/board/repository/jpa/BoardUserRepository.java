ce8181f60f05dd7669bdbde0d30a298459b6bcf2
Hertz <Absolab@github.com>
2023-06-20 09:35:26 +0900

    주석 추가

--- server/src/main/java/kr/co/dtaas/test/board/repository/jpa/BoardUserRepository.java
+++ server/src/main/java/kr/co/dtaas/test/board/repository/jpa/BoardUserRepository.java
@@ -11,9 +11,11 @@ import kr.co.dtaas.test.board.dto.BoardUserVO;
 
 public interface BoardUserRepository extends JpaRepository<BoardUserVO, Long> {
 
-    @Query("SELECT new BoardUserVO(b.bid, b.title, b.crtnDate, b.mdfdDate, u.name) FROM BoardDto b JOIN UserDto u ON b.uid = u.uid WHERE b.deleted = 0")
-    ArrayList<BoardUserVO> findAllBoardWithUserName(Pageable pageable);
-
+    // 글 한개
     @Query("SELECT new BoardUserVO(b.bid, b.title, b.content, b.crtnDate, b.mdfdDate, u.name) FROM BoardDto b JOIN UserDto u ON b.uid = u.uid WHERE b.bid=:bid AND b.deleted = 0")
     BoardUserVO findOneBoardWithUserNameByBid(@Param("bid") int bid);
+
+    // 글 목록
+    @Query("SELECT new BoardUserVO(b.bid, b.title, b.crtnDate, b.mdfdDate, u.name) FROM BoardDto b JOIN UserDto u ON b.uid = u.uid WHERE b.deleted = 0")
+    ArrayList<BoardUserVO> findAllBoardWithUserName(Pageable pageable);
 }
