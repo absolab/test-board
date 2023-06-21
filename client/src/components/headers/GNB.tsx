bfc11149ae52c89801aacfc4b55d04c95df74302
Hertz <kljj0650@naver.com>
2023-06-21 10:29:24 +0900

    홈 누르면 홈으로 가요

--- client/src/components/headers/GNB.tsx
+++ client/src/components/headers/GNB.tsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 const GNB = () => {
+
+    const navigate = useNavigate();
 
     return (
         <div className="flex flex-row">
-            <span className="flex min-w-fit mx-5 my-1 justify-center items-center font-bold w-auto lext-lg cursor-pointer">Home</span>
+            <span className="flex min-w-fit mx-5 my-1 justify-center items-center font-bold w-auto lext-lg cursor-pointer" onClick={() => navigate('/')}>Home</span>
             <span className="flex min-w-fit mx-5 my-1 justify-center items-center font-bold w-auto lext-lg cursor-pointer">Guide</span>
         </div>
     )
